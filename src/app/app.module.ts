3dc22f284200a8e94be9a2bc2939b45809b6baf7
tony_217 <www.anthonyhenriquez@gmail.com>
2019-07-26 23:04:15 -0500

    cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { Routes,RouterModule } from '@angular/router';
+
+// añadir los componentes
+import { LoginComponent } from './login/login.component';
+import { FormComponent } from './form/form.component';
+import { PrincipalComponent } from './principal/principal.component';
+
+// añadir servicio
+import {GetdataService} from './service/getdata.service';
+// importar el modulo http
+import {HttpClientModule  } from '@angular/common/http';
+
+// importar modulos de formulario
+import { FormsModule} from '@angular/forms';
+
+
+
+
+
+// declarar las rutas
+const routes: Routes = [
+  
+  {path:'login', component:LoginComponent},
+  {path:'form', component:FormComponent},
+ 
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+    LoginComponent,
+    FormComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, //añadir el http modulo
+    FormsModule, // añadir formularios y modelos
+    RouterModule.forRoot(routes), // añadir las rutas
+    
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [GetdataService], // añadir los servicios
+  bootstrap: [PrincipalComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  
+ }
---
+++ src/app/service/getdata.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+//importar servicios http
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+
+ // importar el observable
+import { Observable } from 'rxjs/Observable';
+
+
+
+//definir la url principal 
+const  URL_principal = 'http://localhost/apiSql/service/';
+
+@Injectable()
+export class GetdataService {
+
+  // inyectar el httoclient
+  constructor(private httpClient: HttpClient) { }
+
+    // metodo para obtener registros
+    AllUser():Observable<any>{
+    return this.httpClient.get(URL_principal+'user/lista');
+
+  }
+
+ 
+  
+
+
+
+
+
+}
+
+
+
+
