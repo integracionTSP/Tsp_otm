63043448171b9e0179108f4a296a4d9eb2cde93a
Anthony Henriquez <ajhen217@gmail.com>
2019-07-31 17:33:34 -0500

    varios cambios

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -16,42 +16,160 @@ export class FormComponent implements OnInit {
   formPowerDriverGID: FormGroup;
 
   //rutas disponibles 
-  enableAvailableRoutes: boolean = true;
-  // otras rutas 
-  enableOtherRoutes: boolean = true;
+  enableAvailableRoutes: boolean = false;
 
-  // captura de datos de los input
-  powerDriverGID: any = { powerGID: '', driverGID: '' }
+  // otras rutas 
+  enableOtherRoutes: boolean = false;
+
+  // btn imprimir
+  enableBtnPrint: boolean = false;
+
+  // btn aceptar
+  enableBtnAcept: boolean = true;
+
+  // captura de datos de los inzut
+  powerDriverGID: any = { powerGID: 'SXV600', driverGID: '84457569' }
+
+  // mensaje no encontrado
+  notFoundMessage: String = "No existe movimiento con datos ingresados";
+
+  //resultados de la busqueda
+  powerDriverGIDResult: any = {};
+
+  //resultados distintos rutas de la busqueda
+  powerDriverGIDDistResult: any = {};
+
+  // viaje seleccionado de la tabla rutas disponibles
+  selectAvailableRoutesChk: any = {}
+
+  // viaje seleccionado de la tabla rutas disponibles
+  selectOtherRoutesChk: any = {}
+
+
+
+  constructor(private GetdataService: GetdataService, private router: Router, public fb: FormBuilder) {
+
+    this.formPowerDriverGID = this.fb.group({
+      powerGID: ['', [Validators.required]],
+      driverGID: ['', [Validators.required]]
+    });
+
+
+  }
+
+
+  // limpiar campos
+  cleanInput(): void {
+    this.powerDriverGID = { powerGID: '', driverGID: '' }
+  }
 
 
   // buscar por placa e identificacion
 
   searchPowerDriver(powerDriverGID: any): void {
-    
-       //hbailitar rutas disponibles 
-    this.enableAvailableRoutes = true;
-    
-   
 
 
-    // habilitar otras rutas 
-    this.enableOtherRoutes = true;
+    if (powerDriverGID.powerGID && powerDriverGID.driverGID) {
+
+      this.GetdataService.searchPowerDriver(powerDriverGID).subscribe(result => {
+        // capturar los datos de la url
+        this.powerDriverGIDResult = result.response;
+
+        // validar que el registro exista
+        if (this.powerDriverGIDResult != this.notFoundMessage) {
+          this.enableAvailableRoutes = true;
+          console.log(this.powerDriverGIDResult);
+
+        } else {
+          console.log(this.powerDriverGIDResult);
+          this.enableAvailableRoutes = false;
+        }
+
+
+      }, error => {
+        console.log(JSON.stringify(error));
+      }
+
+      );
+
+    } else {
+      console.log('rellene los campos');
+      this.enableAvailableRoutes = false;
+      this.enableOtherRoutes = false;
+      this.enableBtnPrint = false;
+
+    }
+
+
     console.log(powerDriverGID);
+
 
 
   }
 
 
 
-  constructor(private GetdataService: GetdataService, private router: Router, public fb: FormBuilder) {
+  // seleccionar el viaje disponible individual de la tabla rutas disponibles
+  availableRouteSelect(AvailableRoutesChk: any): void {
+    this.enableOtherRoutes = false;
 
-      this.formPowerDriverGID = this.fb.group({
-        powerGID :['',[Validators.required]],
-        driverGID:['',[Validators.required]]
-      });
+    this.enableBtnPrint = true;
+
+ 
+
+    this.selectAvailableRoutesChk = AvailableRoutesChk;
+    console.log("ruta disponible seleccionada ", this.selectAvailableRoutesChk);
+
+  }
+
+  // DESTINOS DISTINTOS A LOS ASOCIADOS
+
+  searchDistPowerDriver(powerDriverGID: any): void {
+
+    this.enableBtnPrint = false;
+    if (powerDriverGID.powerGID && powerDriverGID.driverGID) {
+
+      this.GetdataService.searchDistPowerDriver(powerDriverGID).subscribe(result => {
+
+        this.powerDriverGIDDistResult = result.response;
+
+        // validar que el registro exista
+        if (this.powerDriverGIDDistResult != this.notFoundMessage) {
+          this.enableOtherRoutes = true;
+          console.log("resultados distintos ", this.powerDriverGIDDistResult);
+
+        } else {
+          console.log(this.powerDriverGIDDistResult);
+          this.enableOtherRoutes = false;
+        }
+
+        this.enableOtherRoutes = true;
 
 
-   }
+
+      }, error => {
+
+        console.log(JSON.stringify(error));
+      }
+
+
+      );
+
+
+
+    }
+
+
+  }
+
+
+  // seleccionar otros  individual de la tabla rutas disponibles
+  otherRouteSelect(otherRoutesChk: any): void {
+    this.enableBtnPrint = true;
+    this.selectOtherRoutesChk = otherRoutesChk;
+    console.log("otra ruta seleccionada ", this.selectOtherRoutesChk);
+
+  }
 
   ngOnInit() {
   }
--- src/app/service/ordenCargaService/getdata.service.ts
+++ src/app/service/ordenCargaService/getdata.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 //importar servicios http
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
- // importar el observable
+// importar el observable
 import { Observable } from 'rxjs/Observable';
 
 
 
 //definir la url principal 
-const  URL_principal = 'http://localhost:3000/api/ordenCarga/';
+const URL_principal = 'http://localhost:3000/api/ordenCarga/';
 
 
 
@@ -18,18 +18,21 @@ export class GetdataService {
   // inyectar el httoclient
   constructor(private httpClient: HttpClient) { }
 
-    // metodo para obtener registros
-    AllUser():Observable<any>{
-    return this.httpClient.get(URL_principal+'getAllUserPass');
+  // metodo para obtener registros
+  AllUser(): Observable<any> {
+    return this.httpClient.get(URL_principal + 'getAllUserPass');
 
   }
 
-  searchPowerDriver(powerDriverGID: any):Observable<any>{
+  searchPowerDriver(powerDriverGID: any): Observable<any> {
+
+    return this.httpClient.get(URL_principal+`getAsociados/${powerDriverGID.powerGID}/${powerDriverGID.driverGID}`);
+  }
 
 
-    
-    return this.httpClient.get(URL_principal+'getAsociados/'+powerDriverGID);
+  searchDistPowerDriver(powerDriverGID: any): Observable<any> {
 
+    return this.httpClient.get(URL_principal +`getDistintos/${powerDriverGID.powerGID}/${powerDriverGID.driverGID}`);
   }
 
 
